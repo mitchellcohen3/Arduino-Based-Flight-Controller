bf28c22b4ccee2b4d383af120517ee1396afebd9
Robert Joy <33035630+robertjoy95@users.noreply.github.com>
2017-11-26 12:10:49 -0500

    Update Flight_Controller_V1.0.ino

--- Flight_Controller_V1.0/Flight_Controller_V1.0.ino
+++ Flight_Controller_V1.0/Flight_Controller_V1.0.ino
@@ -33,14 +33,20 @@ double x0, y0;  //define these as the origin, now have a frame of reference for 
 double nlo, nla; //current position in long, lat
 double nx, ny;  //current position in cartesian coords
 double px=0, py=0; //previous position, set to zero because that will be our prev position in the first heading calculation
+double desired_roll = 0;
 int counter = 0;   //counts the current setpt
-struct setpt setpoints[3];  //define an array of setpoints to reach, once we have reached the last point, initiate landing
+struct setpt setpoints[4];  //define an array of setpoints to reach, once we have reached the last point, initiate landing
 
 
 void initialize_setpoints(){
-    setpoints[0].x = x0 + mtolong(20, y0);
-    setpoints[0].y = y0 + mtolat(20);   //placeholder until we come up with an actual coordinate scheme
-    
+    setpoints[0].x = 20;   //setpts are stored in meters
+    setpoints[0].y = 20;   //placeholder until we come up with an actual coordinate scheme
+    setpoints[1].x = 0;   
+    setpoints[1].y = 0;
+    setpoints[2].x = 20;   
+    setpoints[2].y = 20;
+    setpoints[3].x = 0;   
+    setpoints[3].y = 0;
 }    
 
 void setup() {
