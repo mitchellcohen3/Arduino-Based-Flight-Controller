a3ae4c1128fb4b3dfd7b587f4704b24eb8a7aa95
Robert Joy <33035630+robertjoy95@users.noreply.github.com>
2017-11-27 13:10:18 -0500

    Update B_Initialize_Sensors.ino

--- Flight_Controller_V1.0/B_Initialize_Sensors.ino
+++ Flight_Controller_V1.0/B_Initialize_Sensors.ino
@@ -39,7 +39,7 @@ void initialize_GPS(){
     // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
     GPS.begin(9600);
     //turn on RMC (recommended minimum) and GGA (fix data) including altitude
-    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY); 
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); 
     GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
     GPS.sendCommand(PGCMD_ANTENNA);      
     //useInterrupt(false);  //option to have a timer interrupt go off and read data every 1 millisecond 
