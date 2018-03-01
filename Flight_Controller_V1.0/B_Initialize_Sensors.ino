bb3288fdd28c1e12809de3da2eeaa198ebbbca1e
Robert Joy <33035630+robertjoy95@users.noreply.github.com>
2018-03-01 10:31:19 -0500

    Update B_Initialize_Sensors.ino

--- Flight_Controller_V1.0/B_Initialize_Sensors.ino
+++ Flight_Controller_V1.0/B_Initialize_Sensors.ino
@@ -19,6 +19,34 @@ void initialize_GPS(){
   #endif
 } 
 
+#ifdef __AVR__
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;  
+    // writing direct to UDR0 is much much faster than Serial.print 
+    // but only one character can be written at a time. 
+#endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+#endif //#ifdef__AVR__
+uint32_t timer = millis();
 
 
 
